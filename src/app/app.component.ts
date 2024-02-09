8f10011093a3b7533bce3182e688961cdd80bec5
Me <2274252@cegepmontpetit.ca>
2024-02-09 13:56:10 -0500

    FCT _Recherche Albums
    Modification de la méthode searchArtist pour qu'elle recherche les albums à la place des artistes. Création de la classe Album, pour qu'elle stocke les données reçues à partir des données reçues par le API. Celle-ci stocke le nom, l'url de l'image et le nom de l'artiste de cet album. Les données des Albums sont ensuite affiché à l'aide d'une ngFor qui récupère chacun des albums stocker dans une liste d'Albums nommée "albums". Une nouvelle recherche supprime toutes les informations des albums de l'artiste.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { lastValueFrom } from 'rxjs';
+import { Album } from './models/Album';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,10 @@ export class AppComponent {
   // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
   // Une variable devra être ajoutée ici
   // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-  result : boolean = false;
+  resultArtist : boolean = false;
   artist : string = "";
-  similarArtists : string [] = [];
+  albums: Album [] = [];
+  
 
 
   // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
@@ -26,23 +28,25 @@ export class AppComponent {
   }
 
   async searchArtist():Promise<void>{
-    this.result = true;
+    this.resultArtist = true;
     
     // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 	  // La requête HTTP devra être ajoutée ici
     // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-    let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+this.artist+"&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"))
-    for(let artist of x.similarartists.artist ){
-      this.similarArtists.push(artist.name) 
-      console.log(artist.image)
-      
+
+    let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+this.artist+"&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"))
+    for(let a of x.topalbums.album){
+      this.albums.push(new Album(a.name, a.image[3]['#text'], a.artist.name));
     }
-    
+    this.artist = x.topalbums.album[0].artist.name;
+    console.log(this.albums)
 
   }
 
   newSearch():void{
-    this.result = false;
-    this.similarArtists.splice(0, this.similarArtists.length)
+    this.resultArtist = false;
+    this.albums.splice(0, this.albums.length);
   }
+
+
 }
